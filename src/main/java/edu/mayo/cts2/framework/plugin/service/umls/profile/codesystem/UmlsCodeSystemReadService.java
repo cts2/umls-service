a99a59e31feed7b9d23ba49c315af595d2c04220
Kevin Peterson <kevin.peterson@mayo.edu>
2013-02-20 14:15:00 -0600

    Added UmlsCodeSystemReadService.

---
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/umls/profile/codesystem/UmlsCodeSystemReadService.java
@@ -0,0 +1,84 @@
+/*
+* Copyright: (c) 2004-2013 Mayo Foundation for Medical Education and
+* Research (MFMER). All rights reserved. MAYO, MAYO CLINIC, and the
+* triple-shield Mayo logo are trademarks and service marks of MFMER.
+*
+* Except as contained in the copyright notice above, or as used to identify
+* MFMER as the author of this software, the trade names, trademarks, service
+* marks, or product names of the copyright holder shall not be used in
+* advertising, promotion or otherwise in connection with this software without
+* prior written authorization of the copyright holder.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package edu.mayo.cts2.framework.plugin.service.umls.profile.codesystem;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import edu.mayo.cts2.framework.model.codesystem.CodeSystemCatalogEntry;
+import edu.mayo.cts2.framework.model.command.ResolvedReadContext;
+import edu.mayo.cts2.framework.model.core.OpaqueData;
+import edu.mayo.cts2.framework.model.core.SourceReference;
+import edu.mayo.cts2.framework.model.service.core.DocumentedNamespaceReference;
+import edu.mayo.cts2.framework.model.service.core.NameOrURI;
+import edu.mayo.cts2.framework.service.profile.codesystem.CodeSystemReadService;
+
+@Component
+public class UmlsCodeSystemReadService implements CodeSystemReadService {
+
+	@Override
+	public String getServiceName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OpaqueData getServiceDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServiceVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SourceReference getServiceProvider() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DocumentedNamespaceReference> getKnownNamespaceList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CodeSystemCatalogEntry read(NameOrURI identifier,
+			ResolvedReadContext readContext) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean exists(NameOrURI identifier, ResolvedReadContext readContext) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
