f985bb4cd2099aca7b694d5b2e08c896a8ec5ea8
dksharma <sharma.deepak2@mayo.edu>
2013-03-12 14:48:21 -0500

    CodeSystemReadServiceTestIT updated

---
+++ src/test/groovy/edu/mayo/cts2/framework/plugin/service/umls/profile/codesystem/UmlsCodeSystemReadServiceTestIT.groovy
@@ -0,0 +1,57 @@
+package edu.mayo.cts2.framework.plugin.service.umls.profile.codesystem
+
+import static org.junit.Assert.*
+
+import javax.annotation.Resource
+import javax.xml.transform.stream.StreamResult
+
+import org.junit.Test
+
+import edu.mayo.cts2.framework.core.xml.Cts2Marshaller
+import edu.mayo.cts2.framework.model.util.ModelUtils
+import edu.mayo.cts2.framework.plugin.service.umls.test.AbstractTestITBase
+
+class UmlsCodeSystemReadServiceTestIT extends AbstractTestITBase {
+
+	@Resource
+	UmlsCodeSystemReadService service
+
+	@Resource
+	Cts2Marshaller marshaller
+	
+	def doRead() {
+		def cs = service.read(ModelUtils.nameOrUriFromName("LNC"), null)
+	
+		cs
+	}
+	
+	@Test
+	void testSetUp(){
+		assertNotNull service
+	}
+
+	@Test
+	void testRead()
+	{
+		def csn = service.read(ModelUtils.nameOrUriFromName("LNC"), null)
+		assertNotNull csn
+		println "found " + csn.getCodeSystemName()
+	}
+
+	@Test
+	void testExists()
+	{
+		assertTrue service.exists(ModelUtils.nameOrUriFromName("LNC"), null)
+		assertFalse service.exists(ModelUtils.nameOrUriFromName("CODESYSTEMNOTTHERE"), null)
+	}
+
+	
+	@Test
+	void TestValidXml() {
+		def cs = doRead()
+
+		assertNotNull cs
+
+		marshaller.marshal(cs, new StreamResult(new StringWriter()))
+	}
+}
