9afaa2620a70bfe61b3518a2bb0d39037b53c53e
Z Lian <lian.zonghui@mayo.edu>
2013-03-21 12:10:06 -0500

    implement two methods

--- src/main/java/edu/mayo/cts2/framework/plugin/service/umls/profile/codesystem/UmlsCodeSystemQueryService.java
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/umls/profile/codesystem/UmlsCodeSystemQueryService.java
@@ -1,7 +1,12 @@
 package edu.mayo.cts2.framework.plugin.service.umls.profile.codesystem;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.elasticsearch.index.query.QueryBuilder;
+
+import edu.mayo.cts2.framework.filter.match.StateAdjustingPropertyReference;
 import edu.mayo.cts2.framework.model.codesystem.CodeSystemCatalogEntry;
 import edu.mayo.cts2.framework.model.codesystem.CodeSystemCatalogEntrySummary;
 import edu.mayo.cts2.framework.model.command.Page;
@@ -11,21 +16,37 @@ import edu.mayo.cts2.framework.model.core.PropertyReference;
 import edu.mayo.cts2.framework.model.core.SortCriteria;
 import edu.mayo.cts2.framework.model.directory.DirectoryResult;
 import edu.mayo.cts2.framework.plugin.service.umls.profile.AbstractUmlsBaseService;
+import edu.mayo.cts2.framework.plugin.service.umls.profile.entity.EntityQueryStateUpdater;
+import edu.mayo.cts2.framework.service.meta.StandardMatchAlgorithmReference;
+import edu.mayo.cts2.framework.service.meta.StandardModelAttributeReference;
 import edu.mayo.cts2.framework.service.profile.ResourceQuery;
 import edu.mayo.cts2.framework.service.profile.codesystem.CodeSystemQueryService;
 
 public class UmlsCodeSystemQueryService extends AbstractUmlsBaseService implements CodeSystemQueryService{
-
+	
+//	private EntityQueryStateUpdater stateUpdater = new EntityQueryStateUpdater();
+	private CodeSystemQueryStateUpdater stateUpdater = new CodeSystemQueryStateUpdater();
+	
 	@Override
-	public Set<? extends MatchAlgorithmReference> getSupportedMatchAlgorithms() {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<MatchAlgorithmReference> getSupportedMatchAlgorithms() {
+		return new HashSet<MatchAlgorithmReference>(
+			Arrays.asList(StandardMatchAlgorithmReference.CONTAINS.getMatchAlgorithmReference()));
 	}
 
 	@Override
 	public Set<? extends PropertyReference> getSupportedSearchReferences() {
-		// TODO Auto-generated method stub
-		return null;
+		Set<StateAdjustingPropertyReference<QueryBuilder>> returnSet = 
+				new HashSet<StateAdjustingPropertyReference<QueryBuilder>>();
+			
+			PropertyReference resourceSynopsisRef = 
+				StandardModelAttributeReference.RESOURCE_SYNOPSIS.getPropertyReference();
+			
+			StateAdjustingPropertyReference<QueryBuilder> ref = 
+				StateAdjustingPropertyReference.toPropertyReference(resourceSynopsisRef, this.stateUpdater);
+			
+			returnSet.add(ref);
+			
+			return returnSet;
 	}
 
 	@Override
